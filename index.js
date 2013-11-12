705070feadbbc216d713c701aefc1ea0386272bd
paulkav1@gmail.com <paulkav1@gmail.com>
2013-11-12 09:48:07 -0500

    Timeline!

--- index.js
+++ index.js
@@ -23,27 +23,17 @@ function make_timeline(){
   for (var i = 0; i < activities.length; i++){
       item = activities[i];
       p1 = map_log(item["start_time"]);
-      p2 = map_log(item["end_time"]);      
-      ht_str += '<div class="happening" id="' + item["id"] + '">' + item["id"] + ':' + p1 + ',' + p2 + ' ==> ' + item["title"] + '</div>';
-      css_item = {id:item["id"], left:p1, right:p2};
-      css_items.push(css_item);
+      p2 = map_log(item["end_time"]); 
+      w = Math.max((p2 - p1), 35); 
+      t = 150 + (16 * i);    
+      ht_str += '<div style="font-size:9px; font-style:times; position:fixed; background-color:maroon; color:white; left:' + p1 + 'px; width:' + w + 'px; top:' + t + 'px" class="happening" id="' + item["id"] + '">' + item["title"] + '</div>';
   };
   $('#canvas').html(ht_str);
-
-  for (var i = 0; i < css_items.length; i++){
-      line = "'#" + css_items[i].id + "'";
-      $('#e6').css("background-color", "yellow");
-      $('#e6').css('left', "'" + item.left + "px'");
-      var x = item.right - item.left;
-      $('#e6').width(x);      
-      //$(line).css('left', item.left);
-      //$(line).css('length', item.right - item.left);
-  }
 }
 
 //Map years to log scale given 1953 = 0, 2013 = 900
 function map_log(t){
   tx = 2014 - t;
-  ltx = Math.log(tx) * 218;
-  return Math.floor(900 - ltx);
+  ltx = Math.log(tx) * 220;
+  return Math.floor(960 - ltx);
 }
