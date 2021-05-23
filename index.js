43e901ef1431a2a671b542dbf3f61047fa41f044
Paul Kavanagh <paulkav1@gmail.com>
2021-05-23 15:34:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
           activities[i].desc = descriptions[activities[i].id];
           descs[activities[i].id] = activities[i]
       }
-      //$('#ac').accordionPro({ theme:'light', horizontalHeight:600, horizontalWidth:900 }); 
       make_timeline(descs);
 
       $('.happening').mouseover(function(event) {
@@ -44,47 +43,35 @@ function build_detail(d){
 
     if (d.logo !== undefined){
         detail += '<img class="log" src="' + d.logo + '"> ';
-    }else{ 
+    } else {
         detail += '<h2>' + d.title + '</h2>';
-    };               
+    }
 
     detail += d.desc;
-    if (d.pic !== undefined){ 
+    if (d.pic !== undefined) {
         detail += '<img class="pic" src="' + d.pic + '">';
-
-    return detail;
     }
+    return detail;
 }
 
 function make_timeline(descs){
   var ht_str = '';
   var order = 0;
-  var css_items = []
+  var css_items = [];
     for (item in descs){
-      dim = plot_data(descs[item].start_time, descs[item].end_time, order);
+      dim = plot_data(order);
       ht_str += '<div style="font-size:14px; border:1px solid grey; font-family:times; padding:2px; position:fixed; '
         + 'background-color:ivory; color:black; border-radius:3px; left:' + dim.l + 'px; width:' 
         + dim.w + 'px; top:' + dim.t + 'px" class="happening" id="' + descs[item].id + '">' + descs[item].title + '</div>';
       order += 1;
-  };
+  }
   $('#canvas').html(ht_str);
 }
 
-//Map events to log scale given 1953 = 0, 2014 = 900
-function plot_data(start, end, order){
-  var e1 = Math.floor(1400 - (Math.log(2021 - start) * 280));
-  var e2 = Math.floor(1400 - (Math.log(2021 - end) * 280));
-  noise = (Math.random() * 20) - 10;
-  var dim = {};
-  if (e2 - e1 < 70){
-    dim.w = 70; 
-    dim.l = e1 - 60;
-  } else {
-    dim.w = e2 - e1;
-    dim.l = e1;
-  }
-  dim.l += noise;
-  dim.t = 140 + (45 * (order % 12));
- 
-  return dim;
+function plot_data(order){
+    var dim = {};
+    dim.w = 200;
+    dim.l = 100;
+    dim.t = 130 + (30 * order);
+    return dim;
 }
